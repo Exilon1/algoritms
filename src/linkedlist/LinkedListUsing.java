ebc8353214698cc1bd648183c7652386c1030224
Aleksey Nikotin <exilonone@gmail.com>
2022-12-24 13:53:27 +0400

    Task 8

---
+++ src/linkedlist/LinkedListUsing.java
@@ -0,0 +1,42 @@
+package linkedlist;
+
+public class LinkedListUsing {
+    public static void main(String[] args) {
+        LinkedList list1 = new LinkedList();
+        list1.addInTail(new Node(2));
+        list1.addInTail(new Node(3));
+        list1.addInTail(new Node(4));
+
+        LinkedList list2 = new LinkedList();
+        list2.addInTail(new Node(5));
+        list2.addInTail(new Node(6));
+        list2.addInTail(new Node(7));
+
+        LinkedList list = merge(list1, list2);
+
+        Node node = list.head;
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+    public static LinkedList merge(LinkedList firstList, LinkedList secondList) {
+        LinkedList target = new LinkedList();
+
+        if (firstList.count() == secondList.count()) {
+            Node firstNode = firstList.head;
+            Node secondNode = secondList.head;
+            while (firstNode != null) {
+                target.addInTail(new Node(firstNode.value + secondNode.value));
+
+                firstNode = firstNode.next;
+                secondNode = secondNode.next;
+            }
+
+        }
+
+        return target;
+    }
+
+}
