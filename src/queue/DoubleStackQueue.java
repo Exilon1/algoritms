fd110369f0d8b05b4285f29e99f7c31d5037f634
Aleksey Nikotin <exilonone@gmail.com>
2023-01-10 13:38:16 +0400

    Optimization

--- src/queue/DoubleStackQueue.java
+++ src/queue/DoubleStackQueue.java
@@ -6,40 +6,27 @@ import stack.Stack;
 public class DoubleStackQueue<T> {
 
     private final Stack<T> forwardStack = new Stack<>();
-    private final InvertedStack<T> reverseStack = new InvertedStack<>();
-
-    private boolean forwardIsActive = true;
+    private final InvertedStack<T> reversedStack = new InvertedStack<>();
 
     public void enqueue(T item) {
-        moveToForward();
-
         forwardStack.push(item);
     }
 
     public T dequeue() {
-        moveToReverse();
-        return reverseStack.pop();
+        if (reversedStack.size() == 0) {
+            fillReversedStack();
+        }
+
+        return reversedStack.pop();
     }
 
     public int size() {
-        if (forwardIsActive) {
-            return forwardStack.size();
-        }
-
-        return reverseStack.size();
+        return forwardStack.size() + reversedStack.size();
     }
 
-    private void moveToForward() {
-        while (reverseStack.size() > 0) {
-            forwardStack.push(reverseStack.pop());
-        }
-        forwardIsActive = true;
-    }
-
-    private void moveToReverse() {
+    private void fillReversedStack() {
         while (forwardStack.size() > 0) {
-            reverseStack.push(forwardStack.pop());
+            reversedStack.push(forwardStack.pop());
         }
-        forwardIsActive = false;
     }
 }
